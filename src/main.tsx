9c3d6b29c789221bbb0bf942d0a261795f7958d5
sauravpanda <sgp65@cornell.edu>
2026-04-10 14:54:28 -0700

    fix: consolidate CSS imports from main.tsx into styles.css (#282)

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import "./styles/sidebar.css";
-import "./styles/terminal.css";
-import "./styles/command-palette.css";
-import "./styles/dashboard.css";
-import "./styles/status-bar.css";
-import "./styles/project-tabs.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
